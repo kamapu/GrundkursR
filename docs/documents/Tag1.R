cdfe601e73d7ad9a8d4dde60f17a1b93315d2873
Miguel Alvarez <kamapu78@gmail.com>
2023-02-28 23:21:39 +0100

    Commit from git2r

--- docs/documents/Tag1.R
+++ docs/documents/Tag1.R
@@ -1,43 +1,116 @@
-# In den Ordnern navigieren ----
-
-# Das ist ein kommentar
-getwd() # Der Arbeitsverzeichnis
-?getwd
-
-# Mit absoluten Pfaden
-setwd("/home/rkurs/rstudio-projekte/MeinProjekt/Beispiel")
-getwd()
-
-setwd("/home/rkurs/rstudio-projekte/MeinProjekt")
-getwd()
-
-# Mit relativen Pfaden
-setwd("Beispiel")
-getwd()
-
-setwd("../")
-getwd()
-
-setwd("Beispiel/B1")
-setwd("../../")
-getwd()
-
-# Jetzt was Anderes ----
-
-#setwd("/home/rkurs/rstudio-projekte/MeinProjekt/Beispiel")
-setwd("Beispiel")
-
+# Tag 1 ----
 # R als Taschenrechner ----
 
-5 - 200
-5^2
-5**2
+# Dieses ist ein Kommentar
+5 + 5
+5*1000 # Das ist gleich Fünf-Tausend
+100-3
+60/3
 
-25^(1/2)
-25^0.5
-25^.5
-5 +                        50
+# Jetzt wird es ernst ----
 
-5 -
-  6 +
-  10
+10*100 + 5
+10*(100 + 5)
+(10*(100 + 5)) +50
+
+5+5
+5 + 5
+5 +                5
+
+6 *
+  5 + 10
+
+# Zuweisung ----
+
+M <- 500
+M/100
+
+meinWert <- 200
+my_value <- 100
+meinWert + M
+
+M <- 20
+
+M <- 100 * 33
+M + m
+meinwert
+m <- 20
+meinWert
+
+# Logische Operatoren -----
+
+100 > 5
+99 < 5
+
+100 == 100
+100 != 100
+
+100 < 100
+100 <= 100
+
+# Vektor ----
+rm(list = ls())
+
+M <- 23
+N <- 24
+rm(M)
+ls()
+
+rm(list = ls())
+
+meinVektor <- c(2, 3, 4, 5)
+meinVektor*10
+
+M <- c(1:100)
+M
+
+A <- sample(x = 1:100, size = 5)
+A
+
+B <- rep(x = 5, times = 10)
+B
+c(1:1000000)/10
+
+## Vektoren-Klassen ----
+
+### numeric ----
+D <- c(1:100)/1000
+
+### character ----
+A <- "Miguel"
+Teilnehmer <- c("Max", "Aditi", "Linda", "Casey",
+                "Karoline", "Alexander")
+
+Teilnehmer/2
+Zahlen <- c(B, D)
+Zahlen
+Sachen <- c(B, Teilnehmer)
+"5" + "5"
+
+## Coersion ----
+as.character(5)
+as.numeric("5")
+as.numeric("Miguel")
+
+NA # Non-available
+
+is.character(Teilnehmer)
+is.character(B)
+
+## Länge ----
+
+length(Teilnehmer)
+
+## Indizierung ----
+Teilnehmer[3]
+Teilnehmer[-3]
+Teilnehmer[length(Teilnehmer)]
+Teilnehmer[Teilnehmer != "Alexander"]
+Teilnehmer[c(2, 4)]
+
+N <- Teilnehmer != "Alexander"
+class(N)
+is.logical(N)
+c(TRUE, FALSE)
+c(T, F, F)
+c(true, false)
