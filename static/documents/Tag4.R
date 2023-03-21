1566b4e660f9b61044cfbf2e29c6b062c40c041f
Miguel Alvarez <kamapu78@gmail.com>
2023-03-21 21:16:12 +0100

    Skript Tag 4

--- static/documents/Tag4.R
+++ static/documents/Tag4.R
@@ -1,32 +1,71 @@
-# demos, vignettes, examples ----
-demo()
-demo("graphics")
+# Pakete ----
+update.packages(ask = FALSE)
 
-browseVignettes()
-
-?mean
-example("mean")
-
-# Packages ----
-library(ggplot2)
-qplot(1:10, rnorm(10), colour = runif(10))
-
-# ggplot2::qplot()
-
+library("ade4")
 install.packages("ade4")
 
+data(ecomor)
+?taxo2phylog
 
-# Dateien herunterladen ----
-download.file(url = "https://kamapu.github.io/RKurs-VHS-2022/documents/KursDateien.zip",
+library("ade4")
+
+data(ecomor)
+?taxo2phylog
+
+sessionInfo()
+
+# Lesen und Schreiben ----
+download.file(url = "https://kamapu.github.io/GrundkursR/documents/KursDateien.zip",
               destfile = "KursDateien.zip", method = "curl")
 unzip("KursDateien.zip", overwrite = TRUE)
 unlink("KursDateien.zip")
 
-# Sauberes Workspace
+Afrika <- read.csv("Africa.env.csv")
+#Afrika2 <- read.csv2("Africa.env.csv")
+
+write.csv2(Afrika, file = "Afrika.de.csv")
+
+Bonn2019 <- read.csv("Bevoelkerung-2019.csv")
+head(Bonn2019)
+
+summary(Bonn2019$FrauenProzent)
+Bonn2019$FrauenProzent <-
+  gsub(pattern = ",",
+       replacement = ".",
+       x = Bonn2019$FrauenProzent)
+Bonn2019$FrauenProzent <-
+  as.numeric(Bonn2019$FrauenProzent)
+
+install.packages("readODS")
+
+library(readODS)
+
+Bonn2019 <- read_ods("Bevoelkerung.ods")
+
+# Objekten teilen ----
+
 rm(list = ls())
-?read.csv
 
-Bevoelkerung <- read.csv("Bevoelkerung-2021.csv")
-B2 <- read.csv2("Bevoelkerung-2021.csv")
+Afrika <- read.csv("Africa.env.csv")
+Afrika$Country <- as.factor(Afrika$Country)
+summary(Afrika$Country)
 
-write.csv2(Bevoelkerung, "B2.csv", na = "-999999")
+data("iris")
+
+save(Afrika, iris, file = "meineObjekte.rda")
+saveRDS(Afrika, file = "nurAfrika.rds")
+
+rm(list = ls())
+
+# Scenario 1
+Afrika <- readRDS("nurAfrika.rds")
+Afrika <- Afrika[,1]
+
+load("meineObjekte.rda")
+
+# Scenario 2
+rm(list = ls())
+load("meineObjekte.rda")
+Afrika2 <- readRDS("nurAfrika.rds")
+
+
