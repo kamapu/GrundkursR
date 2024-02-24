34ee6762cc030474a388dd4a8f7ba53da477ec16
Miguel Alvarez <kamapu78@gmail.com>
2024-02-24 11:25:01 +0100

    Schummelzettel added.

--- rmakdown/docs/documents/MeineFunktionen.R
+++
@@ -1,7 +0,0 @@
-BonnStats <- function(x, ...) {
-  if (!is.numeric(x))
-    stop("Der Wert 'x' muss numerisch sein!")
-  out <- c(mean(x, ...), sd(x, ...))
-  names(out) <- c("Mittelwert", "Standardabweichung")
-  return(out)
-}
